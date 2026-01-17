5aa68dae076f14836adbb0abeb69124905f99485
ale <145934588+alexandrejulio1410@users.noreply.github.com>
2026-01-17 20:15:01 -0300

    Atualiza projeto

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Postagem } from './../entities/postagem.entity';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Postagem } from '../entities/postagem.entity';
+import { ILike, Repository } from 'typeorm';
+import { DeleteResult } from 'typeorm/browser';
 
 @Injectable()
 export class PostagemService {
@@ -11,6 +12,48 @@ export class PostagemService {
   ) {}
 
   async findAll(): Promise<Postagem[]> {
-    return this.postagemRepository.find();
+    return await this.postagemRepository.find();
+  }
+  async findById(id: number): Promise<Postagem> {
+    const Postagem = await this.postagemRepository.findOne({
+      where: {
+        id
+      }  
+    });
+
+    if(!Postagem)
+      throw new HttpException('Postagem nao encotrada!', HttpStatus.NOT_FOUND);
+
+    return Postagem;
+  }
+
+
+  async findByTitulo(titulo: string): Promise<Postagem[]> {
+    return await this.postagemRepository.find({
+      where: {
+        titulo: ILike(`%${titulo}%`)
+      },
+    });
+  }
+
+  async create(Postagem: Postagem): Promise<Postagem>{
+    return await this.postagemRepository.save(Postagem);
+ }
+
+  async update(Postagem: Postagem): Promise<Postagem> {
+    const buscaPostagem: Postagem = await this.findById(Postagem.id);
+    if(!buscaPostagem || !Postagem.id)
+      throw new HttpException('Postagem nao encotrada!', HttpStatus.NOT_FOUND);
+    
+    return await this.postagemRepository.save(Postagem);
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+
+    const buscaPostagem: Postagem = await this.findById(id);
+    if(!buscaPostagem)
+      throw new HttpException('Postagem nao encotrada!', HttpStatus.NOT_FOUND);
+
+    return await this.postagemRepository.delete(id);
   }
 }
